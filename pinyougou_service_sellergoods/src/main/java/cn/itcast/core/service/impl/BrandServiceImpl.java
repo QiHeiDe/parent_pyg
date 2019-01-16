0e307267f068be13ec0fab6c18d49519f4eb7544
root <qq397380012@163.com>
2019-01-16 15:29:42 +0800

    sellerGoods添加了一个代码

--- pinyougou_service_sellergoods/src/main/java/cn/itcast/core/service/impl/BrandServiceImpl.java
+++ pinyougou_service_sellergoods/src/main/java/cn/itcast/core/service/impl/BrandServiceImpl.java
@@ -20,6 +20,7 @@ public class BrandServiceImpl implements BrandService {
      */
     @Override
     public List<Brand> findAll() {
+        System.out.println("============");
         return brandDao.selectByExample(null);
     }
 }
